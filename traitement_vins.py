2a8228736a3314e305fcb2b8f1cb013fb27dae4f
florian.biggio.dev@gmail.com <florian.biggio.dev@gmail.com>
2025-05-09 15:45:30 +0200

    init project

---
+++ traitement_vins.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Chargement des fichiers Excel
+web = pd.read_excel("data/Fichier_web.xlsx")
+erp = pd.read_excel("data/Fichier_erp.xlsx")
+liaison = pd.read_excel("data/fichier_liaison.xlsx")
